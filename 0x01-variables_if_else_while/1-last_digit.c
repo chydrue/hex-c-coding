ba9cb963dcafce108d37dcc42730add87afa1c30
chydrue <ckmoraiz@gmail.com>
2021-09-01 10:31:49 -0300

    Mostra o último digito de um número aleatório

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - função principal
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n;
+	int p;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n >= 0)
+	{
+		p = n % 10;
+	}
+	else
+	{
+		p = (n % 10)*-1;
+	}
+	
+	if (p > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, p);
+	}
+
+	if (p == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, p);
+	}
+
+	if (p < 6 && p != 0)
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, p);
+	}
+
+	return (0);
+}

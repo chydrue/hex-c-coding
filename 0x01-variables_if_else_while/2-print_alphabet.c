71a3a0e353025a9664c2f771caf011ecbec46ae8
chydrue <ckmoraiz@gmail.com>
2021-09-01 11:03:16 -0300

    Print alfabeto minúsculo utilizando putchar()

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Função principal
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+
+	return (0);
+}
+

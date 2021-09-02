f05e3a17dd8c8b74cb6d7d9c01aa924bf1cb9551
chydrue <ckmoraiz@gmail.com>
2021-09-02 09:21:56 -0300

    Print números base 10 usando putchar()

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Função principal
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+
+	return (0);
+}

258cc1cbebc798e67431a7670af8edc63aed6742
chydrue <ckmoraiz@gmail.com>
2021-09-03 11:04:56 -0300

    Retorna o valor absoluto do último dígito de um número

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+/**
+ * print_last_digit - Escreve o último dígito de um número
+ * @num: número que será utilizado
+ * Return: último dígito
+ */
+int print_last_digit(int num)
+{
+	int lastDig;
+
+	lastDig = num % 10;
+
+	if (num < 0)
+	{
+		lastDig *= -1;
+		_putchar(lastDig + '0');
+		return (lastDig);
+	}
+	else if (num < 0)
+	{
+		_putchar(lastDig + '0');
+		return (lastDig);
+	}
+	else
+	{
+		_putchar(num + '0');
+		return (num);
+	}
+}

c64075378cd212dc507955af4d2ad5a420ba5fc1
chydrue <ckmoraiz@gmail.com>
2021-09-13 15:01:12 -0300

    Função que soma dois inteiros

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * add - soma dois inteiros
+ * @x: primeiro inteiro
+ * @y: segundo inteiro
+ * Return: int 0 quando terminar sem erros
+ */
+
+int add(int x, int y)
+{
+	return (x + y);
+}
